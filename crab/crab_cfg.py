4ce0ef65d9d7d888d01cded9a893f0c1f8eea797
Andreas Albert <andreas.albert@cern.ch>
2019-02-19 18:09:20 +0100

    Monojet

--- crab/crab_cfg.py
+++ crab/crab_cfg.py
@@ -1,33 +1,126 @@
+
 from WMCore.Configuration import Configuration
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 
-config = Configuration()
+import md5
+import sys
+import re
 
-config.section_("General")
-config.General.requestName = 'NanoPost1'
-config.General.transferLogs=True
-config.section_("JobType")
-config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'PSet.py'
-config.JobType.scriptExe = 'crab_script.sh'
-config.JobType.inputFiles = ['crab_script.py','../scripts/haddnano.py'] #hadd nano will not be needed once nano tools are in cmssw
-config.JobType.sendPythonFolder	 = True
-config.section_("Data")
-config.Data.inputDataset = '/DYJetsToLL_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17NanoAOD-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/NANOAODSIM'
-#config.Data.inputDBS = 'phys03'
-config.Data.inputDBS = 'global'
-config.Data.splitting = 'FileBased'
-#config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 2
-config.Data.totalUnits = 10
+def get_dataset():
+    # Parse dataset from commandline
+    # Stolen from https://gitlab.cern.ch/nsmith/monoZ/blob/master/crab/crab.py
+    dataset = 'dummy'
+    for arg in sys.argv:
+        if 'Data.inputDataset=' in arg:
+            dataset = arg.split('=')[1]
+    if dataset == 'dummy':
+        raise Exception("Must pass dataset argument as Data.inputDataset=...")
+    return dataset
 
-config.Data.outLFNDirBase = '/store/user/%s/NanoPost' % (getUsernameFromSiteDB())
-config.Data.publication = False
-config.Data.outputDatasetTag = 'NanoTestPost'
-config.section_("Site")
-config.Site.storageSite = "T2_DE_DESY"
 
-#config.Site.storageSite = "T2_CH_CERN"
-#config.section_("User")
-#config.User.voGroup = 'dcms'
+def base_configuration():
+    config = Configuration()
 
+    config.section_("General")
+    config.General.transferLogs=True
+    config.section_("JobType")
+    config.JobType.pluginName = 'Analysis'
+    config.JobType.psetName = 'PSet.py'
+    config.JobType.scriptExe = 'crab_script.sh'
+    config.JobType.inputFiles = ['../scripts/haddnano.py'] #hadd nano will not be needed once nano tools are in cmssw
+    config.JobType.sendPythonFolder	 = True
+    config.section_("Data")
+    config.Data.inputDBS = 'global'
+    config.Data.splitting = 'FileBased'
+    config.Data.unitsPerJob = 1
+    # config.Data.totalUnits = 10
+
+    config.Data.publication = False
+    config.section_("Site")
+    config.Site.storageSite = "T2_CH_CERNBOX"
+
+    return config
+
+# Ensure that the string is not longer than 100 characters
+# To comply with the CRAB request name limit
+def cut_string(string_in):
+    l = len(string_in)
+    if l < 100:
+        return string_in
+
+    partial_hash = md5.md5(string_in[90:]).hexdigest()
+    ret = string_in[:90] + partial_hash[:10]
+    return ret
+
+def short_name(dataset):
+    _, name, conditions, _ = dataset.split("/")
+
+    # Remove useless info
+    name = name.replace("_TuneCP5","")
+    name = name.replace("_TuneCUETP8M1","")
+    name = name.replace("_13TeV","")
+    name = name.replace("-pythia8","")
+    name = name.replace("madgraphMLM","MLM")
+    name = name.replace("amcnloFXFX","FXFX")
+    name = name.replace("powheg","pow")
+
+    # Detect extension
+    m=re.match(".*(ext\d+).*",conditions);
+    if m:
+        name = name + "_" + m.groups[0]
+
+    if "RunIIFall17" in conditions:
+        name = name + "_2017"
+    elif 'RunIIAutumn18' in conditions:
+        name = name + "_2018"
+
+    return name
+
+tag = "test_v3"
+dataset = get_dataset()
+name = short_name(dataset)
+config = base_configuration()
+
+
+### Determine what to do based on type of dataset
+is_mc = dataset.endswith("SIM")
+crab_script = "crab_script_monojet.py"
+
+
+if ("Autumn18" in dataset) or ("Run2018" in dataset):
+    year=2018
+elif ("Run2017" in dataset) or ("Fall17" in dataset):
+    year=2017
+
+if not is_mc:
+    if year==2017:
+        config.JobType.inputFiles.append("input/json/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt")
+    elif year==2018:
+        config.JobType.inputFiles.append("input/json/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt")
+
+config.JobType.inputFiles.append(crab_script)
+config.JobType.inputFiles.append('keep_and_drop_monojet.txt')
+
+# Pass the dataset name as an argument so that
+# the script can write it into the output files.
+config.JobType.scriptArgs = ["dataset={}".format(name),
+                             "ismc={}".format(is_mc),
+                             "year={}".format(year)]
+
+config.General.requestName = cut_string("nano_post_{0}_{1}".format(tag, name))
+config.Data.inputDataset = dataset
+config.Data.unitsPerJob = 1
+
+if "test" in tag:
+    config.Data.totalUnits = 1
+else:
+    config.Data.totalUnits = -1
+
+config.Data.outputDatasetTag = name
+config.Data.outLFNDirBase = '/store/user/{0}/nanopost/{1}/{2}'.format(getUsernameFromSiteDB(),
+                                                                          tag,
+                                                                          "MC" if is_mc else "Data")
+
+
+config.General.workArea = "./wdir/{}".format(tag)
+config.JobType.allowUndistributedCMSSW = True
---
+++ crab/crab_script_monojet.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+import os
+import sys
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import * 
+
+#this takes care of converting the input files from CRAB
+from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis, addDatasetTag
+
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import *
+
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetUncertainties import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jecUncertainties import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer import *
+
+# Loop over input arguments
+options = {}
+prefixes = ['dataset', 'ismc', 'year']
+for argument in sys.argv:
+    for prefix in prefixes:
+        if argument.startswith(prefix):
+            value = argument.split('=')[-1]
+            options[prefix] = value
+options['ismc'] = options['ismc'].lower() == "true"
+
+# files = inputFiles()
+files = ['file:/eos/user/a/aalbert/nanopost/nopost/2EC2FE2C-56B0-584D-9149-463719420BE5.root']
+branchsel = "keep_and_drop_monojet.txt"
+cut = ''
+
+modules = []
+if options['ismc']:
+    if options['year'] == '2017':
+        modules = [
+            jetmetUncertainties2017(),
+            jetmetUncertainties2017AK8Puppi(),
+            ]
+    elif options['year'] == '2018':
+        modules = [
+            jetmetUncertainties2018(),
+            jetmetUncertainties2018AK8Puppi(),
+            ]
+
+    p = PostProcessor(
+        outputDir=".",
+        inputFiles=files,
+        cut=cut,
+        outputbranchsel=branchsel,
+        modules=modules,
+        provenance=True,
+        fwkJobReport=True)
+else:
+    if options['year'] == '2017':
+        json = "Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt"
+    elif options['year'] == '2018':
+        json = "Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt"
+    p=PostProcessor(outputDir=".",
+        inputFiles=files,
+        cut=cut,
+        outputbranchsel=branchsel,
+        provenance=True,
+        fwkJobReport=True,
+        jsonInput=json)
+p.run()
+
+addDatasetTag()
+
+print "DONE"
+os.system("ls -lR")
+
