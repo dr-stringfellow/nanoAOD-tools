e2fc4431fa9786915afd10205b729fcad4260558
Andreas Albert <andreas.albert@cern.ch>
2019-02-19 18:09:20 +0100

    Monojet
    7Jul19 skim
    
    Add photon variables
    
    Write CRAB output to EOS
    
    Slightly tighter preselection
    
    Update campaign tag
    
    Add submission script
    
    Update preselection
    
    Update submitter.
    
    Tighter preselection
    
    Update preselection
    
    More selection
    
    Change tag
    
    Fixup monojetPost
    
    collectionMerger: Only write active branches
    
    Add testing
    
    Make sure output is filtered.
    
    Fix preselection
    
    Fix preselection
    
    16Jul19
    
    Update crab_cfg
    
    Add newpmx to data set name
    
    Crab sciprt: remove useless log spam
    
    Add PU weights.
    
    Location dependent settings + datasets
    
    17Aug19 skim.
    
    Make sure to keep prefiring weight
    
    Make jet/met pt cuts JES-safe
    
    Add non-isolated electron trigger.
    
    Filter events after JES computation.
    
    21Aug19 Skim
    
    Rework crab_script and preselection
    
    crab_cfg: add nofilter option
    
    Update newpmx label
    
    crab_cfg: increase default job runtime
    
    Keep non-iso ele trigger
    
    Check for branch existence before access
    
    Add EWK V data sets
    
    Add VBF signals to datasets
    
    Add 2016 data sets
    
    More ttbar datasets
    
    Add 2016 photon cutbased ID
    
    Add 2016 ttbar
    
    Do not filter gen particles
    
    Update datasets
    
    Rework preselection
    
    Keep more electron and photon properties
    
    cleanup selectors
    
    Remove b jet veto
    
    Update datasets
    
    Update 2016 triggers
    
    Optimize keep/drop
    
    Rework crab_script + new 2018 JES/JER
    
    Update datasets
    
    15Oct skim
    
    Update datasets
    
    Update keep/drop
    
    Improve variation safe filtering
    
    Use metv2 for 2017
    
    Update datasets
    
    Update MET
    
    26Oct19 skim
    
    Fixup selection
    
    FatJetUncertainties: Do not create unused branches
    
    27Oct19 skim
    
    Remove debug output
    
    Keep nLHE*
    
    25Nov19 Skim
    
    keep LHEPart
    
    26Nov19 Skim
    
    Update datasets
    
    09Dec19 Skim
    
    Deal with differetn dataset versions
    
    Deactivate object filtering.
    
    06Jan20 skim
    
    Fixup 2016 data jet sorting
    
    Fixup 2016 ele trigger.
    
    submit: fixup missing newline

---
+++ crab/crab_script_monojet.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+import os
+import sys
+import re
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import *
+
+#this takes care of converting the input files from CRAB
+from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis, addDatasetTag
+
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.monojet.monojetpost import monojetPost, variation_safe_pt_cut
+from PhysicsTools.NanoAODTools.postprocessing.modules.monojet.triggerselector import triggerSelector
+
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetUncertainties import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.PrefireCorr import PrefCorr
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.collectionMerger import collectionMerger
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import puAutoWeight_2016, puAutoWeight_2017, puAutoWeight_2018
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2 import createJMECorrector
+
+
+def filter_electrons(electron):
+    return (electron.pt>9.9) and (electron.cutBased > 0)
+
+def filter_photons(photon):
+    if hasattr(photon, 'cutBasedBitmap'):
+        return photon.cutBasedBitmap > 0
+    if hasattr(photon, 'cutBased17Bitmap'):
+        return photon.cutBased17Bitmap > 0
+    return False
+
+def filter_genpart(genpart):
+    good = False
+    good |= genpart.status in [1,2,62]
+    good |= (abs(genpart.pdgId) in [11,12,13,14,15,16,22,23,24])
+    return good
+
+def extract_period(dataset):
+    m = re.match('.*201\d([A-Z])', dataset)
+    if not m:
+        raise RuntimeError("Could not extract run period from dataset: " + dataset)
+    return m.groups()[0]
+
+def filter_muons(muon):
+    return (muon.pt>9.9) and (muon.looseId) and (muon.pfRelIso04_all < 0.4)
+
+def met_branch_name(year, jet_type):
+    if (year == '2017') and (jet_type=='AK4PFchs'):
+        return "METFixEE2017"
+    else:
+        return "MET"
+
+jsons = {
+        '2016': "Cert_271036-284044_13TeV_ReReco_07Aug2017_Collisions16_JSON.txt",
+        '2017': "Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt",
+        '2018': "Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt"
+}
+
+def read_options():
+    # Loop over input arguments
+    options = {}
+    prefixes = ['dataset', 'ismc', 'year','nofilter','test','file']
+    for argument in sys.argv:
+        for prefix in prefixes:
+            if argument.startswith(prefix):
+                value = argument.split('=')[-1]
+                options[prefix] = value
+    for p in prefixes:
+        if p not in options.keys():
+            options[p] = ''
+
+    options['ismc'] = options['ismc'].lower() == "true"
+    options['nofilter'] = options['nofilter'].lower() == "true"
+    options['test'] = options['test'].lower() == "true"
+
+    return options
+
+def main():
+    options = read_options()
+
+    if options['test']:
+        files = [options['file']]
+        maxEntries = 1000
+    else:
+        files = inputFiles()
+        maxEntries = 0
+    branchsel = "keep_and_drop_monojet.txt"
+
+    if options['nofilter']:
+        common_modules = []
+        selectors = []
+        mc_selectors = []
+        trigger_selector = []
+    else:
+        triggerfile = 'triggers_nano_v5.txt'
+        trigger_selector = [triggerSelector(triggerfile)]
+        common_modules = [monojetPost()]
+
+        if options['year'] == '2016' and not options['ismc']:
+            jetsorter = lambda x: x.pt
+        else:
+            jetsorter = lambda x: x.pt_nom
+        selectors = [
+            # collectionMerger(input=["Jet"],output="Jet", selector={"Jet" : lambda x : variation_safe_pt_cut(x,19.9)}),
+            # collectionMerger(input=["FatJet"],output="FatJet", selector={"FatJet" : lambda x : variation_safe_pt_cut(x,150.0)}),
+            # collectionMerger(input=["Muon"],output="Muon", selector={"Muon" : filter_muons}),
+            # collectionMerger(input=["Electron"],output="Electron", selector={"Electron" : filter_electrons}),
+            # collectionMerger(input=["Photon"],output="Photon", selector={"Photon" : filter_photons}),
+            collectionMerger(input=["Jet"],output="Jet",sortkey=jetsorter),
+            collectionMerger(input=["FatJet"],output="FatJet",sortkey=jetsorter),
+        ]
+
+        mc_selectors = [
+            collectionMerger(input=["GenPart"],output="GenPart", selector={"GenPart" : filter_genpart}),
+            collectionMerger(input=["GenJet"],output="GenJet", selector={"GenJet" : lambda x : x.pt>20}),
+            collectionMerger(input=["GenJetAK8"],output="GenJetAK8", selector={"GenJetAK8" : lambda x : x.pt>20})
+        ]
+
+    if options['ismc']:
+        jme_modules = []
+        for jet_type in ['AK4PFchs', 'AK8PFPuppi']:
+            jme_modules.append(
+                               createJMECorrector(
+                                                  isMC=True,
+                                                  dataYear=options['year'],
+                                                  jesUncert="Total",
+                                                  redojec=options['year']=='2018',
+                                                  jetType=jet_type,
+                                                  metBranchName=met_branch_name(options['year'], jet_type)
+                                                  )()
+                                )
+        if options['year'] == '2016':
+            pu_modules = [puAutoWeight_2016()]
+            pref_modules = [
+                            PrefCorr(
+                                    jetroot="L1prefiring_jetpt_2016BtoH.root",
+                                    jetmapname="L1prefiring_jetpt_2016BtoH",
+                                    photonroot="L1prefiring_photonpt_2016BtoH.root",
+                                    photonmapname="L1prefiring_photonpt_2016BtoH")
+                            ]
+        if options['year'] == '2017':
+            pu_modules = [ puAutoWeight_2017() ]
+            pref_modules = [
+                            PrefCorr(
+                                    jetroot="L1prefiring_jetpt_2017BtoF.root",
+                                    jetmapname="L1prefiring_jetpt_2017BtoF",
+                                    photonroot="L1prefiring_photonpt_2017BtoF.root",
+                                    photonmapname="L1prefiring_photonpt_2017BtoF"
+                                    )
+                            ]
+        elif options['year'] == '2018':
+            pu_modules = [ puAutoWeight_2018() ]
+            pref_modules = []
+
+        modules = trigger_selector + jme_modules + common_modules + pu_modules + pref_modules+ selectors + mc_selectors
+        p = PostProcessor(
+            outputDir=".",
+            inputFiles=files,
+            outputbranchsel=branchsel,
+            modules=modules,
+            provenance=True,
+            maxEntries=maxEntries,
+            fwkJobReport=True)
+    else:
+        jme_modules = []
+        if options['year']=='2018' or options['year']=='2017':
+            run_period = extract_period(options['dataset'])
+            for jet_type in ['AK4PFchs', 'AK8PFPuppi']:
+                jme_modules.append(
+                                createJMECorrector(
+                                                    isMC=False,
+                                                    dataYear=options['year'],
+                                                    jesUncert="Total",
+                                                    redojec=True,
+                                                    runPeriod=run_period,
+                                                    jetType=jet_type,
+                                                    metBranchName=met_branch_name(options['year'], jet_type)
+                                                    )()
+                                    )
+
+        modules = trigger_selector + jme_modules + common_modules + selectors
+        p=PostProcessor(outputDir=".",
+            inputFiles=files,
+            outputbranchsel=branchsel,
+            modules=modules,
+            provenance=True,
+            fwkJobReport=True,
+            maxEntries=maxEntries,
+            jsonInput=jsons[options['year']])
+    p.run()
+
+    addDatasetTag()
+
+    print "DONE"
+
+if __name__ == "__main__":
+    main()
+
