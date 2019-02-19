19793b12a93c08c910414c73b7726aae9e6d4181
Andreas Albert <andreas.albert@cern.ch>
2019-02-19 18:09:20 +0100

    Monojet
    7Jul19 skim
    
    Add photon variables
    
    Write CRAB output to EOS
    
    Slightly tighter preselection
    
    Update campaign tag
    
    Add submission script
    
    Update preselection
    
    Update submitter.
    
    Tighter preselection
    
    Update preselection
    
    More selection
    
    Change tag
    
    Fixup monojetPost
    
    collectionMerger: Only write active branches
    
    Add testing
    
    Make sure output is filtered.
    
    Fix preselection
    
    Fix preselection
    
    16Jul19
    
    Update crab_cfg
    
    Add newpmx to data set name
    
    Crab sciprt: remove useless log spam
    
    Add PU weights.
    
    Location dependent settings + datasets
    
    17Aug19 skim.
    
    Make sure to keep prefiring weight
    
    Make jet/met pt cuts JES-safe
    
    Add non-isolated electron trigger.
    
    Filter events after JES computation.
    
    21Aug19 Skim
    
    Rework crab_script and preselection
    
    crab_cfg: add nofilter option
    
    Update newpmx label
    
    crab_cfg: increase default job runtime
    
    Keep non-iso ele trigger
    
    Check for branch existence before access
    
    Add EWK V data sets
    
    Add VBF signals to datasets
    
    Add 2016 data sets
    
    More ttbar datasets
    
    Add 2016 photon cutbased ID
    
    Add 2016 ttbar
    
    Do not filter gen particles
    
    Update datasets
    
    Rework preselection
    
    Keep more electron and photon properties
    
    cleanup selectors
    
    Remove b jet veto
    
    Update datasets
    
    Update 2016 triggers
    
    Optimize keep/drop
    
    Rework crab_script + new 2018 JES/JER
    
    Update datasets
    
    15Oct skim
    
    Update datasets
    
    Update keep/drop
    
    Improve variation safe filtering
    
    Use metv2 for 2017
    
    Update datasets
    
    Update MET
    
    26Oct19 skim
    
    Fixup selection
    
    FatJetUncertainties: Do not create unused branches
    
    27Oct19 skim
    
    Remove debug output
    
    Keep nLHE*
    
    25Nov19 Skim
    
    keep LHEPart
    
    26Nov19 Skim
    
    Update datasets
    
    09Dec19 Skim
    
    Deal with differetn dataset versions
    
    Deactivate object filtering.
    
    06Jan20 skim
    
    Fixup 2016 data jet sorting
    
    Fixup 2016 ele trigger.
    
    submit: fixup missing newline

---
+++ python/postprocessing/modules/monojet/monojetpost.py
@@ -0,0 +1,118 @@
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+import os
+
+jet_pt_names = [
+                'pt',
+                'pt_jesTotalDown',
+                'pt_jesTotalUp',
+                'pt_jerDown',
+                'pt_jerUp',
+                'pt_nom',
+                'pt_raw'
+                ]
+met_pt_names = [
+                'pt',
+                'pt_nom',
+                'pt_jer',
+                'pt_jesTotalDown',
+                'pt_jesTotalUp',
+                'pt_jerDown',
+                'pt_jerUp',
+                'pt_unclustEnUp',
+                'pt_unclustEnDown'
+                ]
+
+
+def variation_safe_pt_cut(obj, cut):
+    for key in set(jet_pt_names + met_pt_names):
+        if not hasattr(obj, key):
+            continue
+        val = getattr(obj, key)
+        if  val > cut:
+            return True
+    return False
+
+class monojetPost(Module):
+    def __init__(self):
+        pass
+    def beginJob(self):
+        pass
+    def endJob(self):
+        pass
+    def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        pass
+    def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        pass
+    def has_jets(self, jets, fatjets):
+        for j in jets:
+            if variation_safe_pt_cut(j, 75):
+                return True
+        for fj in fatjets:
+            if variation_safe_pt_cut(fj, 150):
+                return True
+        return False
+
+    def has_met(self, event):
+        for met in ['MET', 'METFixEE2017']:
+            for key in set(jet_pt_names + met_pt_names):
+                full_key = met+"_"+key
+                if not hasattr(event, full_key):
+                    continue
+                if  getattr(event, full_key)> 75:
+                    return True
+        return False
+
+    def has_muons(self, event):
+        muons = Collection(event,"Muon")
+        for m in muons:
+            if m.pt > 20 and m.looseId and (m.pfRelIso04_all < 0.4):
+                return True
+        return False
+
+    def has_electrons(self, event):
+        electrons = Collection(event,"Electron")
+        for e in electrons:
+            if e.pt > 20 and e.cutBased > 0:
+                return True
+        return False
+
+    def has_photons(self, event):
+        photons = Collection(event,"Photon")
+        for p in photons:
+            if p.pt > 175:
+                return True
+            # and p.electronVeto:
+            #     if hasattr(p,'cutBasedBitmap') and (getattr(p,'cutBasedBitmap') > 0):
+            #         return True
+            #     if hasattr(p,'cutBased17Bitmap') and (getattr(p,'cutBased17Bitmap') > 0):
+            #         return True
+        return False
+
+    def analyze(self, event):
+        """process event, return True (go to next module) or False (fail, go to next event)"""
+        jets = Collection(event,"Jet")
+        # Check for jets
+        fatjets = Collection(event,"FatJet")
+        if not self.has_jets(jets, fatjets):
+            return False
+
+        # All events surviving this far have jets.
+        # Now pass events if they have leptons, a photon or MET
+        if self.has_muons(event):
+            return True
+        if self.has_electrons(event):
+            return True
+        if self.has_photons(event):
+            return True
+
+        # Check for MET
+        if self.has_met(event):
+            return True
+        if event.PuppiMET_pt > 75:
+            return True
+
+        return False
