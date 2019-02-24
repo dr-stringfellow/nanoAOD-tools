06481f87e08fea53c48eaed9c868b4e545cecf4c
Andreas Albert <andreas.albert@cern.ch>
2019-02-24 16:20:43 +0100

    2017

--- crab/crab_script_vbfhinv.py
+++ crab/crab_script_vbfhinv.py
@@ -4,11 +4,17 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import *
 
 #this takes care of converting the input files from CRAB
 from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.muonScaleResProducer import *
+
 from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import *
-# p=PostProcessor(".",inputFiles(),"",branchsel="keep_and_drop_vbf_hinv.txt",provenance=True,fwkJobReport=True)
-# p=PostProcessor(".",inputFiles(),"",modules=[muonScaleRes2017(), puAutoWeight()],provenance=True,fwkJobReport=True)
-p=PostProcessor(".",["/user/albert/scratch/VBF_HToInvisible_M125_13TeV_TuneCP5_powheg_pythia8/823D1180-A3F3-E811-9C4E-E0071B73C600.root"],"",modules=[muonScaleRes2017(), puAutoWeight()],provenance=True,fwkJobReport=True)
+
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetUncertainties import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer import *
+
+modules = [ jetmetUncertainties2017(), jetmetUncertainties2017AK4Puppi(),btagSF2017(),puAutoWeight()]
+
+files = inputFiles()
+files = ["/user/albert/scratch/VBF_HToInvisible_M125_13TeV_TuneCP5_powheg_pythia8/823D1180-A3F3-E811-9C4E-E0071B73C600.root"]
+p=PostProcessor(".",files,"",modules=modules,provenance=True,fwkJobReport=True)
 p.run()
 
 print "DONE"
