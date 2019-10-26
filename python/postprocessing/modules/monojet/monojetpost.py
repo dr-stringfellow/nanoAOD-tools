22c8b9c5daab7c8e290e4f79705b7e9ab33a1f40
Andreas Albert <andreas.albert@cern.ch>
2019-10-26 17:03:37 -0400

    Improve variation safe filtering

--- python/postprocessing/modules/monojet/monojetpost.py
+++ python/postprocessing/modules/monojet/monojetpost.py
@@ -7,6 +7,16 @@ import os
 
 jet_pt_names = ['pt','pt_jesTotalDown','pt_jesTotalUp','pt_jerDown','pt_jerUp','pt_nom','pt_raw']
 met_pt_names = ['pt','pt_jesTotalDown','pt_jesTotalUp','pt_jerDown','pt_jerUp','pt_nom','pt_unclustEnUp','pt_unclustEnDown']
+
+
+def variation_safe_pt_cut(obj, cut):
+    for key in dir(obj):
+        if not obj.startswith('pt_'):
+            continue
+        if getattr(obj, key) > cut:
+            return True
+    return False
+
 class monojetPost(Module):
     def __init__(self):
         pass
@@ -19,50 +29,47 @@ class monojetPost(Module):
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
     def has_jets(self, jets, fatjets):
-        for name in jet_pt_names:
-            for j in jets:
-                if hasattr(j, name):
-                    if getattr(j, name) > 75:
-                        return True
-            for fj in fatjets:
-                if hasattr(fj, name):
-                    if (getattr(fj, name) > 75):
-                            return True
-            return False
-    def has_met(self, event):
-        for name in met_pt_names:
-            branch = 'MET_'+name
-            if hasattr(event, branch):
-                if getattr(event, 'MET_'+name) > 75:
-                    return True
+        for j in jets:
+            if variation_safe_pt_cut(j, 75):
+                return True
+        for fj in fatjets:
+            if variation_safe_pt_cut(fj, 150):
+                return True
         return False
+
+    def has_met(self, event):
+        for key in dir(event):
+            if not (key.startswith("MET") and 'pt' in key):
+                continue
+            if getattr(event, key) > 75:
+                return True
+        return False
+
     def has_muons(self, event):
         muons = Collection(event,"Muon")
         for m in muons:
             if m.pt > 20 and m.looseId and (m.pfRelIso04_all < 0.4):
                 return True
         return False
+
     def has_electrons(self, event):
         electrons = Collection(event,"Electron")
         for e in electrons:
             if e.pt > 20 and e.cutBased > 0:
                 return True
         return False
+
     def has_photons(self, event):
         photons = Collection(event,"Photon")
         for p in photons:
-            if p.pt > 150 and p.electronVeto:
-                if hasattr(p,'cutBasedBitmap') and (getattr(p,'cutBasedBitmap') > 0):
-                    return True
-                if hasattr(p,'cutBased17Bitmap') and (getattr(p,'cutBased17Bitmap') > 0):
-                    return True
+            if p.pt > 175:
+                return True
+            # and p.electronVeto:
+            #     if hasattr(p,'cutBasedBitmap') and (getattr(p,'cutBasedBitmap') > 0):
+            #         return True
+            #     if hasattr(p,'cutBased17Bitmap') and (getattr(p,'cutBased17Bitmap') > 0):
+            #         return True
         return False
-
-    def has_b_jet(self, jets):
-        for j in jets:
-            # tighter than tight WP for both years
-            if j.cleanmask and j.pt > 20 and abs(j.eta) < 2.5 and  j.btagDeepB > 0.81:
-                return False
 
     def analyze(self, event):
         """process event, return True (go to next module) or False (fail, go to next event)"""
