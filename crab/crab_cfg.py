f14ac88e833af3cdf160802805c8e1192bad76f6
Alp Akpinar <aakpinar@bu.edu>
2020-06-11 01:07:31 -0500

    Change the tag to 21May20_v3: WJets memory issue is fixed + central smeared MET is fixed

--- crab/crab_cfg.py
+++ crab/crab_cfg.py
@@ -86,7 +86,7 @@ def short_name(dataset):
 
     return name
 
-tag = "21May20_splitJEC_METcorrected"
+tag = "21May20_splitJEC_v3"
 dataset = get_dataset()
 name = short_name(dataset)
 config = base_configuration()
