cb7c90ed6ad1866460c8196c6262fa6516e42371
Alp Akpinar <aakpinar@bu.edu>
2021-02-22 16:28:08 -0600

    DYv8_22Feb21

--- crab/crab_cfg.py
+++ crab/crab_cfg.py
@@ -92,7 +92,7 @@ def short_name(dataset):
         name = name + "_" + run
     return name
 
-tag = "ULv8_05Feb21"
+tag = "DYv8_22Feb21"
 dataset = get_dataset()
 name = short_name(dataset)
 config = base_configuration()
