3be077ebb5de7317c40ae460f02af277cb7e807c
Alp Akpinar <aakpinar@bu.edu>
2021-02-26 13:59:59 -0600

    Include v8 DY MC sample

--- crab/crab_cfg.py
+++ crab/crab_cfg.py
@@ -105,7 +105,7 @@ crab_script = "crab_script_monojet.py"
 
 if ("Summer16" in dataset) or ("Run2016" in dataset):
     year=2016
-elif ("Run2017" in dataset) or ("Fall17" in dataset):
+elif ("Run2017" in dataset) or ("Fall17" in dataset) or ("Summer20UL17" in dataset):
     year=2017
 if ("Autumn18" in dataset) or ("Run2018" in dataset):
     year=2018
