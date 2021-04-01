e1290f2878fefca7c644a3ec5e8c488c1f93213b
Alp Akpinar <aakpinar@bu.edu>
2021-04-01 10:39:02 -0500

    Setup for 2018 submission

--- python/postprocessing/modules/jme/jetmetHelperRun2.py
+++ python/postprocessing/modules/jme/jetmetHelperRun2.py
@@ -10,6 +10,7 @@ jecTagsMC = { '2016'   : 'Summer16_07Aug2017_V11_MC',
               '2017'   : 'Fall17_17Nov2017_V32_MC',
               '2018'   : 'Autumn18_V19_MC',
               'UL2017' : 'Summer19UL17_V5_MC',
+              'UL2018' : 'Summer19UL18_V5_MC',
             }
 
 jecTagsFastSim = { '2016' : 'Spring16_25nsFastSimV1_MC',
@@ -21,6 +22,7 @@ archiveTagsDATA = { '2016'   : 'Summer16_07Aug2017_V11_DATA',
                     '2017'   : 'Fall17_17Nov2017_V32_DATA',
                     '2018'   : 'Autumn18_V19_DATA',
                     'UL2017' : 'Summer19UL17_V5_DATA', 
+                    'UL2018' : 'Summer19UL18_V5_DATA'
                   }
 
 jecTagsDATA = { '2016B'   : 'Summer16_07Aug2017BCD_V11_DATA',
@@ -44,12 +46,17 @@ jecTagsDATA = { '2016B'   : 'Summer16_07Aug2017BCD_V11_DATA',
                 'UL2017D' : 'Summer19UL17_RunD_V5_DATA',
                 'UL2017E' : 'Summer19UL17_RunE_V5_DATA',
                 'UL2017F' : 'Summer19UL17_RunF_V5_DATA',
+                'UL2018A' : 'Summer19UL18_RunA_V5_DATA',
+                'UL2018B' : 'Summer19UL18_RunB_V5_DATA',
+                'UL2018C' : 'Summer19UL18_RunC_V5_DATA',
+                'UL2018D' : 'Summer19UL18_RunD_V5_DATA',
               }
 
 jerTagsMC = { '2016'   : 'Summer16_25nsV1_MC',
               '2017'   : 'Fall17_V3_MC',
               '2018'   : 'Autumn18_V7b_MC',
               'UL2017' : 'Summer19UL17_JRV2_MC',
+              'UL2018' : 'Summer19UL18_JRV2_MC',
             }
 
 #jet mass resolution: https://twiki.cern.ch/twiki/bin/view/CMS/JetWtagging
@@ -58,6 +65,7 @@ jmrValues = { '2016'   : [1.0, 1.2, 0.8],
               '2017'   : [1.09, 1.14, 1.04],
               '2018'   : [1.09, 1.14, 1.04], # Use 2017 values for 2018 until 2018 are released
               'UL2017' : [1.00, 1.00, 1.00], # placeholder
+              'UL2018': [1.00, 1.00, 1.00],  # placeholder
             }
 
 #jet mass scale
@@ -67,6 +75,7 @@ jmsValues = { '2016'   : [1.00, 0.9906, 1.0094], # nominal, down, up
               '2017'   : [0.982, 0.978, 0.986],
               '2018'   : [0.982, 0.978, 0.986], # Use 2017 values for 2018 until 2018 are released
               'UL2017' : [1.000, 1.000, 1.000], # placeholder
+              'UL2018' : [1.000, 1.000, 1.000], # placeholder
             }
 
 def createJMECorrector(isMC=True, dataYear=2016, runPeriod="B", jesUncert="Total", jetType="AK4PFchs", noGroom=False, metBranchName="MET", applySmearing=True, isFastSim=False, applyHEMfix=False, splitJER=False, isUL=True):
